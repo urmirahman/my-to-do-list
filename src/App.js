b163f5bc791df878aa63444823b98161c7eb1050
urmirahman <urmi.rahman690@gmail.com>
2021-04-01 22:56:49 +0600

    😁:initial login done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
-import { Container ,Button} from "@material-ui/core";
+import { Container, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { useMediaQuery } from "@material-ui/core";
 function App() {
-  const [taskType, setTaskType] = useState([
-    {name:'abe'}, {name:'fsd'}, {name:'abdsfssde'},
-  ]);
-  //localStorage.setItem("lastname", JSON.stringify(taskType));
-  //let separte = localStorage.getItem("lastname").split('][')
-  console.log(JSON.parse(localStorage.getItem("lastname")))
+  const [taskType, setTaskType] = useState([{ name: "" }]);
+  console.log(JSON.parse(localStorage.getItem("lastname")));
+let length_local = JSON.parse(localStorage.getItem("lastname")).length;
+ let test = [{name:'hello'},{name:"world"}]
+ let arrayLenght= test.length
+ 
+ if(length_local > arrayLenght){
+   test = JSON.parse(localStorage.getItem("lastname"))
+ }
+ console.log(test)
   return (
     <div className="App">
-      <Container>{taskType.map((type)=>(<h1>{type.name}</h1>))}
-      <Button onClick={(e)=>{e.preventDefault(); setTaskType(prevtype =>([...prevtype , {name:'new type'}]));  localStorage.setItem("lastname", JSON.stringify(taskType));} } variant="contained" color="primary">Add new</Button>
+      <Container>
+        {test.map((type) => (
+          <h1>{type.name}</h1>
+        ))}
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+           test.push({name:"newworld"});
+           console.log(test)
+            localStorage.setItem("lastname", JSON.stringify(test));
+          }}
+          variant="contained"
+          color="primary"
+        >
+          Add new
+        </Button>
       </Container>
     </div>
   );
