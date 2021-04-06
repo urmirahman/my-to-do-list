4e3150db4817ab5380eaa51b52d1cbf1f70453f3
urmirahman <urmi.rahman690@gmail.com>
2021-04-06 20:27:20 +0600

    🎇:improved

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AddOutlinedIcon from "@material-ui/icons/AddOutlined";
 export const InputText = () => {
   const classes = userStyles();
   return (
     <form>
       <TextField
-          id="outlined-full-width"
-          label="Label"
-          style={{ margin: 8 }}
-          placeholder="Add task"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
+        id="outlined-full-width"
+        label="Add Tasks"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AddOutlinedIcon />
+            </InputAdornment>
+          ),
+        }}
+        style={{ margin: 8 }}
+        placeholder="Add task"
+        fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+      />
     </form>
   );
 };
