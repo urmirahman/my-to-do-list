35c5e372219e1763a0aebfe9f0f520fe1421af49
urmirahman <urmi.rahman690@gmail.com>
2021-04-06 17:39:01 +0600

    file name changed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Rought } from './components/Rought';
+import { Rought } from './components/Rough';
 
 ReactDOM.render(
   <React.StrictMode>
