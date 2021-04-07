cc2848bb18d043376176928d419e90ae615f873b
urmirahman <urmi.rahman690@gmail.com>
2021-04-07 23:10:28 +0600

    ✨:completed task showing

--- src/components/CompletedTask.jsx
+++ src/components/CompletedTask.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
+import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import { Box } from '@material-ui/core';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import { Box } from "@material-ui/core";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
-export const CompletedTask = () => {
-    const classes = useStyles()
-    return (
-        <Box  className={`${classes.box} ${classes.complete}`} p={1} display="flex" bgcolor="blue"alignItems="center">
-        <CheckCircleIcon OnClick className={classes.icon}/>
-        Completed tasks
+export const CompletedTask = ({taskname}) => {
+  const classes = useStyles();
+  return (
+    <Box
+      className={`${classes.box} ${classes.complete}`}
+      p={1}
+      display="flex"
+      bgcolor="blue"
+      alignItems="center"
+    >
+      <CheckCircleIcon OnClick className={classes.icon} />
+      {taskname}
     </Box>
-    )
-}
+  );
+};
 
 const useStyles = makeStyles({
-    box:{
-        background: "#00c9ff", /* fallback for old browsers */
-        background: "-webkit-linear-gradient(to right, #00c9ff, #92fe9d)", /* Chrome 10-25, Safari 5.1-6 */
-        background:" linear-gradient(to right, #00c9ff, #92fe9d)",
-        padding:"10px 15px",
-        borderRadius:5
-        
-    },
-    icon:{
-        padding:"0px 10px"
-    },
-    complete:{
-        textDecoration: "line-through"
-    }
-})
+  box: {
+    background: "#00c9ff" /* fallback for old browsers */,
+    background:
+      "-webkit-linear-gradient(to right, #00c9ff, #92fe9d)" /* Chrome 10-25, Safari 5.1-6 */,
+    background: " linear-gradient(to right, #00c9ff, #92fe9d)",
+    padding: "10px 15px",
+    borderRadius: 5,
+    margin:"10px 0px"
+  },
+  icon: {
+    padding: "0px 10px",
+  },
+  complete: {
+    textDecoration: "line-through",
+  },
+});
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,103 @@
-import { Button } from "@material-ui/core";
+import { Button, Divider, Chip } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
-import { IncompleteTask } from '../components/IncompleteTask';
-import { InputText } from '../components/InputText';
+import { makeStyles } from "@material-ui/styles";
+import { IncompleteTask } from "../components/IncompleteTask";
+import { CompletedTask } from "../components/CompletedTask";
+import { InputText } from "../components/InputText";
 
 export const Home = () => {
-    let value = JSON.parse(localStorage.getItem("tasklist")) === null ? [] : JSON.parse(localStorage.getItem("tasklist"))
-    let value2 = JSON.parse(localStorage.getItem("completeTask")) === null ? [] : JSON.parse(localStorage.getItem("completeTask"))
-    const [incompleteTask, setIncompleteTask] = useState(value);
-    const [completeTask, setCompleteTask] = useState(value2);
-   
-    useEffect(() => {
-        localStorage.setItem("tasklist", JSON.stringify(incompleteTask));
-        //console.log(value)
-        localStorage.setItem("completeTask", JSON.stringify(completeTask));
-        console.log(completeTask)
-         console.log(JSON.parse(localStorage.getItem("tasklist")))
-        //console.log(incompleteTask.length);
-      }, [incompleteTask,completeTask]);
+  const classes = useStyles();
+  let value =
+    JSON.parse(localStorage.getItem("tasklist")) === null
+      ? []
+      : JSON.parse(localStorage.getItem("tasklist"));
+  let value2 =
+    JSON.parse(localStorage.getItem("completeTask")) === null
+      ? []
+      : JSON.parse(localStorage.getItem("completeTask"));
+  const [incompleteTask, setIncompleteTask] = useState(value);
+  const [completeTask, setCompleteTask] = useState(value2);
 
-      const AddComplteTask = (taskname,index) => {
-        setCompleteTask((old) => [...old , { name: taskname }]);
-        incompleteTask.splice(index,1)
-       // console.log(incompleteTask)
-      };
-    
-    const handlekey = (e) => {
-        let temp = e.target.value
-        if(e.key === "Enter"){
-            setIncompleteTask((old) => [...old, { name: temp }])
-            console.log(incompleteTask);
-        }
-        
-    };
-    const handleremove = (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(incompleteTask));
+    localStorage.setItem("completeTask", JSON.stringify(completeTask));
+    console.log(completeTask);
+    console.log(incompleteTask.length);
+  }, [incompleteTask, completeTask]);
+
+  const AddComplteTask = (taskname, index) => {
+    setCompleteTask((old) => [...old, { name: taskname }]);
+    incompleteTask.splice(index, 1);
+  };
+
+  const handlekey = (e) => {
+    let temp = e.target.value;
+    if (e.key === "Enter") {
+      setIncompleteTask((old) => [...old, { name: temp }]);
+      //console.log(incompleteTask);
+    }
+  };
+  const handleremove = (e) => {
+    e.preventDefault();
     //    localStorage.removeItem("tasklist")
-       let p = []
-       setCompleteTask(p)
-      };
+    let p = [];
+    setCompleteTask(p);
+  };
 
-
-    return (
-        <div>
-            <InputText  onkeydown={handlekey}/>
-            {incompleteTask.length > 0 && incompleteTask.map((data,index)=>(<div key={index}><IncompleteTask AddComplteTask={()=>AddComplteTask(data.name,index)} taskname={data.name}/></div>))}
-           <Button onClick={handleremove} variant="contained" color="secondary">remove</Button>
+  return (
+    <div>
+      <InputText onkeydown={handlekey} />
+      {incompleteTask.length > 0 &&
+        incompleteTask.map((data, index) => (
+          <div key={index}>
+            <IncompleteTask
+              AddComplteTask={() => AddComplteTask(data.name, index)}
+              taskname={data.name}
+            />
+          </div>
+        ))}
+      {completeTask.length > 0 && (
+        <div className={classes.dividerTag}>
+          <Chip label="Complete" />{" "}
+          <Divider className={classes.divider} component="li" variant="inset" />
         </div>
-    )
-}
+      )}
+{completeTask.length > 0 &&
+        completeTask.map((data, index) => (
+          <div key={index}>
+            <CompletedTask
+            //   AddComplteTask={() => AddComplteTask(data.name, index)}
+              taskname={data.name}
+            />
+          </div>
+        ))}
+    
+
+      <Button onClick={handleremove} variant="contained" color="secondary">
+        remove
+      </Button>
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  dividerTag: {
+    marginTop: "-10px",
+    padding: "10px 0px",
+    listStyle: "none",
+    zIndex: 20,
+  },
+  divider: {
+    marginLeft: "100px !important",
+    zIndex: 10,
+  },
+
+  "@global": {
+    // You should target [class*="MuiButton-root"] instead if you nest themes.
+    ".MuiChip-root": {
+      margin: "0px 10px !important",
+      height: "20px !important",
+      fontWeight: 600,
+    },
+  },
+});
